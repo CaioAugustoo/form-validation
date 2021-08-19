947f53255432115ce5373051b2ef662b000f0271
caioaugustoo <caioamfr@gmail.com>
2021-08-19 11:50:03 -0300

    Confirmação de senhas iguais

--- index.js
+++ index.js
@@ -1,6 +1,75 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
+var SHOW_MESSAGE_ERROR = "field-error";
 var ValidationFormFields = /** @class */ (function () {
-    function ValidationFormFields() {
+    function ValidationFormFields(selectors) {
+        this.form = document.querySelector(selectors.form);
+        this.password = document.querySelector(selectors.password);
+        this.confirmPassword = document.querySelector(selectors.confirmPassword);
+        this.bindEvents();
+        this.events();
     }
+    ValidationFormFields.prototype.hideMessage = function (field) {
+        var messageElement = field.nextElementSibling;
+        field.classList.remove(SHOW_MESSAGE_ERROR);
+        messageElement.innerText = "";
+        messageElement.style.display = "none";
+    };
+    ValidationFormFields.prototype.comparePasswords = function () {
+        var messageElement = this.confirmPassword;
+        if (this.password.value !== this.confirmPassword.value) {
+            this.showMessage(messageElement, "Senhas não coincidem");
+        }
+    };
+    ValidationFormFields.prototype.checkForEmptyValues = function () {
+        var _this = this;
+        var fields = document.querySelectorAll(".form-fields input");
+        fields.forEach(function (field) {
+            if (!field.value.trim().length) {
+                _this.showMessage(field, "Campo não pode estar em branco");
+            }
+            else {
+                _this.hideMessage(field);
+            }
+        });
+    };
+    ValidationFormFields.prototype.showMessage = function (field, message) {
+        var messageElement = field.nextElementSibling;
+        field.classList.add(SHOW_MESSAGE_ERROR);
+        messageElement.innerText = message;
+        messageElement.style.display = "block";
+    };
+    ValidationFormFields.prototype.isFormValid = function () {
+        var errors = __spreadArray([], document.querySelectorAll(".form-fields .error-message"));
+        var containsErrors = errors.filter(function (el) { return el.innerText; });
+        if (!!containsErrors.length)
+            return false;
+        return true;
+    };
+    ValidationFormFields.prototype.submitForm = function (event) {
+        event.preventDefault();
+        this.checkForEmptyValues();
+        this.comparePasswords();
+        var isValid = this.isFormValid();
+        if (!isValid)
+            return;
+        console.log("Formlulário enviado :)");
+    };
+    ValidationFormFields.prototype.bindEvents = function () {
+        this.submitForm = this.submitForm.bind(this);
+    };
+    ValidationFormFields.prototype.events = function () {
+        var _this = this;
+        this.form.addEventListener("submit", function (event) { return _this.submitForm(event); });
+    };
     return ValidationFormFields;
 }());
+var validation = new ValidationFormFields({
+    form: ".form",
+    password: ".password",
+    confirmPassword: ".password2",
+});
--- index.ts
+++ index.ts
@@ -1,1 +1,101 @@
-class ValidationFormFields {}
+const SHOW_MESSAGE_ERROR = "field-error";
+
+interface ISelectors {
+  form: string;
+  password: string;
+  confirmPassword: string;
+}
+
+class ValidationFormFields {
+  private form: HTMLFormElement;
+  private password: HTMLInputElement;
+  private confirmPassword: HTMLInputElement;
+
+  constructor(selectors: ISelectors) {
+    this.form = document.querySelector(selectors.form) as HTMLFormElement;
+    this.password = document.querySelector(
+      selectors.password
+    ) as HTMLInputElement;
+    this.confirmPassword = document.querySelector(
+      selectors.confirmPassword
+    ) as HTMLInputElement;
+
+    this.bindEvents();
+    this.events();
+  }
+
+  private hideMessage(field: HTMLInputElement): void {
+    const messageElement = field.nextElementSibling as HTMLSpanElement;
+
+    field.classList.remove(SHOW_MESSAGE_ERROR);
+    messageElement.innerText = "";
+    messageElement.style.display = "none";
+  }
+
+  private comparePasswords(): void {
+    const messageElement = this.confirmPassword;
+
+    if (this.password.value !== this.confirmPassword.value) {
+      this.showMessage(messageElement, "Senhas não coincidem");
+    }
+  }
+
+  private checkForEmptyValues(): void {
+    const fields: NodeListOf<HTMLInputElement> =
+      document.querySelectorAll(".form-fields input");
+
+    fields.forEach(field => {
+      if (!field.value.trim().length) {
+        this.showMessage(field, "Campo não pode estar em branco");
+      } else {
+        this.hideMessage(field);
+      }
+    });
+  }
+
+  private showMessage(field: HTMLInputElement, message: string): void {
+    const messageElement = field.nextElementSibling as HTMLSpanElement;
+
+    field.classList.add(SHOW_MESSAGE_ERROR);
+    messageElement.innerText = message;
+    messageElement.style.display = "block";
+  }
+
+  private isFormValid(): boolean {
+    const errors = [
+      ...(document.querySelectorAll(
+        ".form-fields .error-message"
+      ) as any as Array<HTMLElement>),
+    ];
+
+    const containsErrors = errors.filter(el => el.innerText);
+    if (!!containsErrors.length) return false;
+
+    return true;
+  }
+
+  private submitForm(event: Event): void {
+    event.preventDefault();
+
+    this.checkForEmptyValues();
+    this.comparePasswords();
+    const isValid = this.isFormValid();
+    if (!isValid) return;
+
+    console.log("Formlulário enviado :)");
+  }
+
+  private bindEvents(): void {
+    this.submitForm = this.submitForm.bind(this);
+  }
+
+  private events(): void {
+    this.form.addEventListener("submit", event => this.submitForm(event));
+  }
+}
+
+const validation = new ValidationFormFields({
+  form: ".form",
+  password: ".password",
+  confirmPassword: ".password2",
+});
